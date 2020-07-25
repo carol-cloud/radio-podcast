bdba05d216be2a7733aa884e163d752fb4f69768
Geazi Oliveira <geazioliveira@hotmail.com>
2020-07-25 03:19:42 -0300

    feature para tocar de verdade

--- js/podcasts.js
+++ js/podcasts.js
@@ -5,6 +5,8 @@ var realPodcasts = [
     description:
       '[Inglês] Desenvolvimento JavaScript em geral. Apresentado por Wes Bos e Scott Tolinksi',
     img: '1.png',
+    rss: 'https://feed.syntax.fm/rss',
+    url: '',
   },
   {
     id: '88.6',
@@ -12,6 +14,8 @@ var realPodcasts = [
     description:
       'Desenvolvimento JavaScript em geral. Apresentado pelo pessoal da Rocketseat.',
     img: '2.jpg',
+    rss: 'https://anchor.fm/s/3bf2f2c/podcast/rss',
+    url: '',
   },
   {
     id: '92.3',
@@ -19,6 +23,8 @@ var realPodcasts = [
     description:
       'Desenvolvimento e diversidade. Apresentado pelo pessoal da Lambda3',
     img: '3.jpg',
+    rss: 'https://www.lambda3.com.br/feed/podcast/',
+    url: '',
   },
   {
     id: '93.9',
@@ -26,6 +32,8 @@ var realPodcasts = [
     description:
       'Desenvolvimento em geral e histórias de carreira de desenvolvedores.',
     img: '4.png',
+    rss: 'https://devnaestrada.com.br/feed.xml',
+    url: '',
   },
   {
     id: '95.8',
@@ -33,23 +41,31 @@ var realPodcasts = [
     description:
       'Desenvolvimento em geral. Apresentado pelo pessoal da Caelum/Alura.',
     img: '5.png',
+    rss: 'https://hipsters.tech/feed/podcast/',
+    url: '',
   },
   {
     id: '98.3',
     title: 'NerdCast',
     description: 'Cultura Nerd. Apresentado pelo Jovem Nerd e Azhagal.',
     img: '6.jfif',
+    rss: 'https://jovemnerd.com.br/feed-nerdcast',
+    url: '',
   },
   {
     id: '100.4',
     title: 'Área de Transferência',
     description: 'Tecnologia em geral.',
     img: '7.png',
+    rss: 'https://feeds.fireside.fm/adt/rss',
+    url: '',
   },
   {
     id: '105.3',
     title: 'Tecnocast',
     description: 'Tecnologia em geral. Apresentado pelo pessoal do Tecnoblog',
     img: '8.jpg',
+    rss: 'http://feeds.feedburner.com/Tecnocast?format=xml',
+    url: '',
   },
 ];
